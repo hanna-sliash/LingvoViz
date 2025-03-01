8aa97eb5d71c52b8e065b05b7faad73044cb5754
Hanna Sliashynskaya <hanna.sliashynskaya@gmail.com>
2025-03-01 17:15:47 +0100

    fixed formatting

--- src/1Scrape_UNESCO.py
+++ src/1Scrape_UNESCO.py
@@ -1,8 +1,9 @@
+import time
+import unicodedata
+
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
-import unicodedata
-import time
 
 current_page = 0
 # Defining constants
@@ -16,16 +17,16 @@ languages_data = []
 
 # loop through UNESCO website to scrape the information
 proceed = True
-while(proceed == True):
-    url = "https://en.wal.unesco.org/discover/languages?text=&sort_by=title&page="+str(current_page)
+while proceed == True:
+    url = f"https://en.wal.unesco.org/discover/languages?text=&sort_by=title&page={current_page}"
     page = requests.get(url)
     page.encoding = "utf-8"
 
     soup = BeautifulSoup(page.text, "html.parser")
 
-#if 404 Not found error pops up, the loop will stop
+    # if 404 Not found error pops up, the loop will stop
 
-    if soup.title.text == "404 Not Found": 
+    if soup.title.text == "404 Not Found":
         proceed = False
     else:
         all_languages = soup.find_all("div", class_="node__content clearfix")
@@ -33,51 +34,65 @@ while(proceed == True):
         for language in all_languages:
             item = {}
 
-            # Extract the name of the language 
-            name_div = language.find("div", class_="field field--name-field-display-title field--type-string field--label-hidden field__item")
+            # Extract the name of the language
+            name_div = language.find(
+                "div",
+                class_="field field--name-field-display-title field--type-string field--label-hidden field__item",
+            )
             if name_div:
                 name_text = name_div.text.strip()
                 item["Name"] = unicodedata.normalize("NFKD", name_text)
 
-            status_div = language.find("div", class_="field field--name-field-language-situation-m field--type-list-string field--label-hidden field__item")
-            
+            status_div = language.find(
+                "div",
+                class_="field field--name-field-language-situation-m field--type-list-string field--label-hidden field__item",
+            )
+
             # Extract the status of the language
             if status_div:
-                 item["Status"] = status_div.text.strip()
+                item["Status"] = status_div.text.strip()
             else:
-                 item["Status"] = UNKNOWN_VALUE
+                item["Status"] = UNKNOWN_VALUE
 
             # Extract the URL
             link_tag = language.find("a")
             if link_tag and "href" in link_tag.attrs:
-                 item["Link"] = link_tag.attrs["href"]
+                item["Link"] = link_tag.attrs["href"]
             else:
-                print(f"No link found for language; {item.get("Name", UNKNOWN_VALUE)}")
+                print(f"No link found for language; {item.get('Name', UNKNOWN_VALUE)}")
                 continue
 
-            # Extract additional information from language specific pages (number of speakers, glottocode, and country) 
+            # Extract additional information from language specific pages (number of speakers, glottocode, and country)
             # We need to click on the specific language page and extract the information from there
             language_page = requests.get(item["Link"])
             language_soup = BeautifulSoup(language_page.text, "html.parser")
 
             # Number of speakers
-            speakers_div = language_soup.find("div", class_= "field field--name-field-number field--type-integer field--label-hidden field__item")
+            speakers_div = language_soup.find(
+                "div",
+                class_="field field--name-field-number field--type-integer field--label-hidden field__item",
+            )
             if speakers_div:
                 item["Speakers"] = speakers_div.text.strip()
             else:
                 item["Speakers"] = NOT_AVAILABLE
-        
+
             # Glottocode - an international classification number that is unique for each language and will be later used to join the datasets on it
-            glottocode_div = language_soup.find("div", class_ = "field field--name-field-glottocode field--type-string field--label-inline")
+            glottocode_div = language_soup.find(
+                "div",
+                class_="field field--name-field-glottocode field--type-string field--label-inline",
+            )
             if glottocode_div:
                 item["Glottocode"] = glottocode_div.text.strip()[11:]
             else:
                 item["Glottocode"] = NOT_AVAILABLE
 
             # Country
-            country_container = language_soup.find("div", class_="view-countries-associated")
+            country_container = language_soup.find(
+                "div", class_="view-countries-associated"
+            )
             if country_container:
-                country_tag = country_container.find("a", href = True)
+                country_tag = country_container.find("a", href=True)
                 if country_tag:
                     item["Country"] = country_tag.text.strip()
                 else:
@@ -87,8 +102,7 @@ while(proceed == True):
 
             # Save scraped data
             languages_data.append(item)
-  
-    
+
     if current_page > PAGE_LIMIT:
         proceed = False
     print(f"Data from page {current_page} saved")
--- src/4Data_cleaning_final_dataset.py
+++ src/4Data_cleaning_final_dataset.py
@@ -1,12 +1,12 @@
 import numpy as np
 import pandas as pd
 
-#Remove unnamed and Name_y colums from the dataset
+# Remove unnamed and Name_y colums from the dataset
 df = pd.read_csv(r".\Data\final_dataset.csv")
 df.drop(columns=["Unnamed: 0", "Name_y"], inplace=True)
 print(df.dtypes)
 
-#Remove rows were Name_x field is empty
+# Remove rows were Name_x field is empty
 initial_rows = len(df)
 df = df.dropna(subset=["Name_x"])
 
@@ -16,5 +16,5 @@ deleted_rows = initial_rows - final_rows
 
 print(f"{deleted_rows} rows were removed.")
 
-#Resave the file
+# Resave the file
 df.to_csv(r".\Data\final_dataset.csv", index=False)
